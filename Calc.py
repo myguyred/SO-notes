3b18b69fc1d170536bff6680043a9af5c76d5bf8
myguyred <97485380+myguyred@users.noreply.github.com>
2023-10-27 00:18:20 -0400

    Add files via upload
    Calculate stuff, and I'm going to add more to this slowly.
    I'm only in 7th grade I'm not big brain smart yet.

---
+++ Calc.py
@@ -0,0 +1,27 @@
+import math, os
+
+def cls():
+	os.system("cls")
+
+## Main ##
+cls()
+print("1. Velocity")
+print("2. Acceleration")
+
+ans = input("-> ")
+
+if ans == "1":
+	cls()
+	d = int(input("Distance-> "))
+	t = int(input("Time-> "))
+	cls()
+	print(f"Ans: {d/t}")
+
+if ans == "2":
+	cls()
+	vf = int(input("vf-> "))
+	vi = int(input("vi-> "))
+	tf = int(input("tf-> "))
+	ti = int(input("ti-> "))
+	cls()
+	print(f"Ans: {(vf - vi) / (tf - ti)}")
